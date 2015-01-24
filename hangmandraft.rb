98327638ab99d73c3ebb6f54a161adb9a7141504
1UnboundedSentience <jsnx21@gmail.com>
2015-01-23 20:15:32 -0800

    Update hangmandraft.rb
    This version lets you save

--- hangmandraft.rb
+++ hangmandraft.rb
@@ -1,68 +1,93 @@
 class HangmanGame
-	def initialize(word)
-		@word = word
-		@current_word = "_" * (word.size-2)
-	end
-
-	def current_word
-		@current_word
-	end
-
-	def word
-		@word
-	end
-
-	def take_guess(guess_ltr)
-		@word.split("").each_with_index do |word_ltr, i|
-			if word_ltr == guess_ltr.downcase
-				@current_word[i] = guess_ltr.downcase
-			end
-		end
-	end
-	def done? 
-		if @word == @current_word
-			true
-		else
-			false
-		end
-	end
+  def initialize(word)
+    @word=word
+    print word
+    @current_word = "_" * (word.size)
+  end
+  
+  def current_word
+    @current_word
+  end
+  
+  def set_current_word(word)
+    @current_word=word
+  end
+  
+  def word
+    @word
+  end
+  
+  def take_guess(guess_ltr)
+    @word.split("").each_with_index do |word_ltr, i|
+      if word_ltr == guess_ltr.downcase
+        @current_word[i] = guess_ltr.downcase
+      end
+    end
+  end
+  
+  def done?
+    if @word == @current_word
+      true
+    else 
+      false
+    end
+  end
 end
 
 
-thewordlist = []
-file = File.open("5desk.txt")
 
-file.readlines.each do |line|
-	#print line
-	#check if within 5-12 characters
-	thewordlist.push(line)
-end
-item = thewordlist[rand(thewordlist.length)]
-#print item
+puts "Load previous game? Type 'yes' to load"
+gamestatus=gets.chomp
+if gamestatus=="yes"
+  allsavedprogress=File.read('savedfile.txt')
+  
+  progressarray=allsavedprogress.split("\n")
 
-#def add_word
-#	@listofwords ||= Array.new
-#	@listofwords.push(name)
-#	@listofwords
-#end
-#puts add_word
-
-#Last two characters always don't count, they are line breaks
-#use some string splicing to remove them
-
-game = HangmanGame.new(item)
-
-
-15.times do 
-
-	print "Enter your guess of the letter:"
-	inLetter = gets.chomp
-	game.take_guess(inLetter)
-	puts game.current_word
-	if game.done?
-		puts "YOU WIN!"
-		break
-	end
+  game = HangmanGame.new(progressarray[-1])
+  game.set_current_word(progressarray[0])
+  numguesses = progressarray[1].to_i
+else
+  thewordlist = [ ]
+  file = File.open("5desk.txt")
+  file.readlines.each do |line|
+      thewordlist.push(line.chomp)
+  end
+  filtered_list = thewordlist.select {|temp_item|temp_item.length>5 && temp_item.length<12}
+  item = filtered_list[rand(filtered_list.length)]
+  game = HangmanGame.new(item)
+  numguesses=8
 end
 
-puts "And the mystery word was " + item
+
+
+
+
+numguesses.downto(1) do |count|
+  puts "You have " + count.to_s + " guesses left"
+  puts "to save the game. type 'save'"
+  print "Enter your guess of the letter:"
+  userentry = gets.chomp
+  
+
+  game.take_guess(userentry)
+  puts game.current_word
+
+  if userentry.include? 'save'
+   puts "Jason rocks!"
+   savedthings = game.current_word + "\n" + count.to_s + "\n" + game.word
+   puts savedthings
+   File.write('savedfile.txt', savedthings)
+   break
+  end
+  if game.word.include? userentry
+    puts "Correct guess!"
+  else
+    puts "You muppet!  Wrong guess!"
+  end
+  
+  if game.done?
+    puts "You WIN!"
+    break
+  end
+end
+puts "And the mystery word was " + game.word
